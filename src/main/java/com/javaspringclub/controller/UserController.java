eb6cacad9fa79eb2838b1da449757dfe43233bad
Vladimir Filippov <sergeiych@gmail.com>
2019-11-25 04:46:27 +0300

    all works

---
+++ src/main/java/com/javaspringclub/controller/UserController.java
@@ -0,0 +1,115 @@
+package com.javaspringclub.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.javaspringclub.entity.User;
+import com.javaspringclub.service.UserService;
+
+@Controller
+public class UserController {
+	// Constructor based Dependency Injection
+	private UserService userService;
+
+	public UserController() {
+
+	}
+
+	@Autowired
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+
+
+	@RequestMapping(value = { "/", "/allUsers" }, method = RequestMethod.GET, produces = "text/plain;charset=UTF-8")
+	public ModelAndView hello(HttpServletResponse response) throws IOException {
+		ModelAndView mv = new ModelAndView();
+		List<User> userList = userService.getAllUsers();
+		mv.addObject("userList", userList);
+		mv.setViewName("allUsers");
+		return mv;
+	}
+
+	// Get All Users
+	@RequestMapping(value = "/allUsers", method = RequestMethod.POST, produces = "text/plain;charset=UTF-8")
+	public ModelAndView displayAllUser() {
+		System.out.println("User Page Requested : All Users");
+		ModelAndView mv = new ModelAndView();
+		List<User> userList = userService.getAllUsers();
+		mv.addObject("userList", userList);
+		mv.setViewName("allUsers");
+		return mv;
+	}
+
+	@RequestMapping(value = "/addUser", method = RequestMethod.GET, produces = "text/plain;charset=UTF-8")
+	public ModelAndView displayNewUserForm() {
+		ModelAndView mv = new ModelAndView("addUser");
+		mv.addObject("headerMessage", "Add User Details");
+		mv.addObject("user", new User());
+		return mv;
+	}
+
+	@RequestMapping(value = "/addUser", method = RequestMethod.POST, produces = "text/plain;charset=UTF-8")
+	public ModelAndView saveNewUser(@ModelAttribute User user, BindingResult result) {
+
+		ModelAndView mv = new ModelAndView("redirect:/allUsers");
+
+		if (result.hasErrors()) {
+			return new ModelAndView("error");
+		}
+		boolean isAdded = userService.saveUser(user);
+		if (isAdded) {
+//			mv.addObject("message", "New user successfully added");
+		} else {
+			return new ModelAndView("error");
+		}
+
+		return mv;
+	}
+
+	@RequestMapping(value = "/editUser/{id}", method = RequestMethod.GET, produces = "text/plain;charset=UTF-8")
+	public ModelAndView displayEditUserForm(@PathVariable Long id) {
+		ModelAndView mv = new ModelAndView("/editUser");
+		User user = userService.getUserById(id);
+		mv.addObject("headerMessage", "Edit User Details");
+		mv.addObject("user", user);
+		return mv;
+	}
+
+	@RequestMapping(value = "/editUser/{id}", method = RequestMethod.POST, produces = "text/plain;charset=UTF-8")
+	public ModelAndView saveEditedUser(@ModelAttribute User user, BindingResult result) {
+		ModelAndView mv = new ModelAndView("redirect:/allUsers");
+
+		if (result.hasErrors()) {
+			System.out.println(result.toString());
+			return new ModelAndView("error");
+		}
+		boolean isSaved = userService.saveUser(user);
+		if (!isSaved) {
+
+			return new ModelAndView("error");
+		}
+		return mv;
+	}
+
+	@RequestMapping(value = "/deleteUser/{id}", method = RequestMethod.GET, produces = "text/plain;charset=UTF-8")
+	public ModelAndView deleteUserById(@PathVariable Long id) {
+		boolean isDeleted = userService.deleteUserById(id);
+		System.out.println("User deletion respone: " + isDeleted);
+		ModelAndView mv = new ModelAndView("redirect:/allUsers");
+		return mv;
+
+	}
+
+}
